6d0fef903decd960eeda5b0de2ee3cb33fad4126
Pomuch17 <Pomuch17@DESKTOP-KA5IT0B>
2026-03-03 07:14:34 -0600

    feat:estructura inicial del proyecto.

---
+++ src/main/java/com/mycompany/practicagit_william/PracticaGit_William.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practicagit_william;
+
+/**
+ *
+ * @author Pomuch17
+ */
+public class PracticaGit_William {
+
+    public static void main(String[] args) {
+        System.out.println("---Sistema de gestión de Alumnos---");
+    }
+}
