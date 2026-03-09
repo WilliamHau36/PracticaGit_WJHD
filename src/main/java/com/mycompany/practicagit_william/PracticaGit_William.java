06fc98d2ba08df776398883c298b61b20eb961e9
Pomuch17 <Pomuch17@DESKTOP-KA5IT0B>
2026-03-09 07:10:42 -0600

    feat:estructura inicial del proyecto.

--- src/main/java/com/mycompany/practicagit_william/PracticaGit_William.java
+++ src/main/java/com/mycompany/practicagit_william/PracticaGit_William.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.practicagit_william;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Pomuch17
@@ -12,5 +14,37 @@ public class PracticaGit_William {
 
     public static void main(String[] args) {
         System.out.println("---Sistema de gestión de Alumnos---");
+         System.out.println("Bienvenido, Usuario Estudiante.");
+         Scanner cin = new Scanner(System.in);
+           int n1, n2 = 0, suma;
+        
+        
+        
+        System.out.print ("Introduce primer número: ");
+        n1 = cin.nextInt ();
+        
+        
+        System.out.print("Introduce segundo número: ");
+        n2 = cin.nextInt ();
+        
+        
+        
+        
+        suma = n1 + n2 ;
+        
+        
+        
+        
+        System.out.println ("el resultado es: " + suma);
+         
+         
+         
+         
+         
+         
+         
+         
+        
+        
     }
 }
